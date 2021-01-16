2143d8b22b2c465589d61dd52ee007953d9bf704
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-01-16 22:59:09 +0900

    21.01.16

---
+++ CLI/x-mas/codeit/kakao1.java
@@ -0,0 +1,44 @@
+import javax.swing.plaf.synth.SynthEditorPaneUI;
+
+// 1.비밀지도
+// 네오는 평소 프로도가 비상금을 숨겨놓는 장소를 알려줄 비밀지도를 손에 넣었다. 그런데 비밀지도는 숫자로
+// 암호화되어 있어 위치를 확인하기 위해서는 암호는 래독해야 한다. 다행히 지도 암호를 해독할 방법을 적어놓은
+// 메모도 함께 발견했다.
+// 1. 지도는 한 변의 길이가 n인 정사각형 배열 형태로, 각 칸은 "공백" 또는 "벽"("#")두 종류로 이루어져 있다.
+// 2. 전체 지도는 두 장의 지도를 겹쳐서 얻을 수 있다. 각각 "지도 1"과 "지도 2"라고 하자. 지도 1또는 지도2
+//  중 어느 하나라도 벽인 부분은 전테 지도에서도 벽이다. 지도 1과 지도 2에서 모두 공백인 부분은 전체 
+//  지도에서도공백이다.
+// 3. "지도 1"과 "지도 2"는 각각 정수 배열로 암호화되어 있다.
+// 4. 암호화된 배열은 지도의 각 가로중에서 벽 부분을 1, 공백 부분을 0으로 부호화했을 때, 얻어지는 
+// 이진수에 해당하는 값의 배열이다.
+// package kakaotest1;
+
+public class kakaotest1 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+        //["####","# # #", "### #"m "# ##", "#####"]
+        String[] result = new String[n];
+
+        for(int i = 0; i < n; i++) {
+            int arr = arr1[i] | arr2[i];
+            System.out.println("" + Integer.toBinaryString(arr));
+    
+        }
+        System.out.println("");
+
+        for(int i = 0; i < n; i++) {
+            String resultString = "";
+            int targetBit = 1;
+            int arr = arr1[i] | arr2[i];
+            for(int j = 0; j < n; j++) {
+                resultString = (arr & targetBit)>0?"#":"") + resultString;
+                targetBit = targetBit << 1;
+            }
+            result[i] = resultString;
+            System.out.println(result[i]);
+        }
+    }
+}
