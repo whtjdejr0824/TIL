384ff01c66990f3862eb2f6c5353b37400171e5a
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-01-26 22:38:30 +0900

    210126

---
+++ CLI/x-mas/codeit/크롤링/11_daum_movie.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get("https://search.daum.net/search?nil_suggest=btn&w=tot&DA=SBC&q=2019%EB%85%84%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84")
+res.raise_for_status()
+soup = BeautifulSoup(res.text, "lxml")
+
+images = soup.find_all("img", attrs={"class":"thumb_img"})
+
+for image in images:
+    # print(image["src"])
+    image_url = image["src"]
+    if image_url.startswith("//"):
+        image_url = "https:" + image_url
+
+    print(image_url)
