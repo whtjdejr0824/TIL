456568625e9e1012f28ab707e3364517eb867098
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-01-29 23:44:35 +0900

    Update 11_daum_movie.py

--- CLI/x-mas/codeit/크롤링/11_daum_movie.py
+++ CLI/x-mas/codeit/크롤링/11_daum_movie.py
@@ -1,16 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get("https://search.daum.net/search?nil_suggest=btn&w=tot&DA=SBC&q=2019%EB%85%84%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84")
-res.raise_for_status()
-soup = BeautifulSoup(res.text, "lxml")
 
-images = soup.find_all("img", attrs={"class":"thumb_img"})
+for year in range(2015, 2020):
+    url = "https://search.daum.net/search?nil_suggest=btn&w=tot&DA=SBC&q={}%EB%85%84%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84".format(year)
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "lxml")
 
-for image in images:
-    # print(image["src"])
-    image_url = image["src"]
-    if image_url.startswith("//"):
-        image_url = "https:" + image_url
+    images = soup.find_all("img", attrs={"class":"thumb_img"})
 
-    print(image_url)
+    for idx, image in enumerate(images) :
+        # print(image["src"])
+        image_url = image["src"]
+        if image_url.startswith("//"):
+            image_url = "https:" + image_url
+
+        print(image_url)
+        image_res = requests.get(image_url)
+        image_res.raise_for_status()
+
+        with open("movie_{}_{}.jpg".format(year, idx+1), "wb") as f:
+            f.write(image_res.content)
+
+        if idx >= 4: # 상위 5개 이미지까지만 다운로드
+            break
