76d6da10d769f730aa6027985c1b9504562e0233
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-02-05 22:37:38 +0900

    210205

--- CLI/x-mas/codeit/크롤링/15_selenium_movie.py
+++ CLI/x-mas/codeit/크롤링/15_selenium_movie.py
@@ -2,8 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "http://play.google.com/store/movies/top"
-headers = {"User-Agent":Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit}
-
 res = requests.get(url)
 res.raise_for_status()
 soup = BeautifulSoup(res.text, "lxml")
@@ -12,5 +10,4 @@ movies = soup.find_all("div", attrs={"class":"ImZGtf mpg5gc"})
 print(len(movies))
 
 with open("movie.html", "w",encoding="utf-8") as f:
-    # f.write(res.text)
-    f.write(soup.prettify()) # html 문서를 예쁘게 출력
+    f.write(res.text)
