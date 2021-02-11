087a6982c5c47a889c4f15b173dca6e4d37281b8
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-02-11 23:47:14 +0900

    210211

---
+++ CLI/x-mas/case_example.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 134354;
+        // 여기에 코드를 작성하세요.
+        switch (number % 7) { // i : 불린이 아닌 식, 변수, 메소드
+        case 0:
+            System.out.println("Zero");
+        break;
+        case 1:
+            System.out.println("One");
+        break;
+        case 2:
+            System.out.println("Two");
+        break;
+        case 3:
+            System.out.println("Three");
+        break;
+        case 4:
+            System.out.println("Four");
+        break;
+        case 5:
+            System.out.println("Five");
+        break;
+        case 6:
+            System.out.println("Six");
+        break;
+        default:
+        }
+    }
+}
