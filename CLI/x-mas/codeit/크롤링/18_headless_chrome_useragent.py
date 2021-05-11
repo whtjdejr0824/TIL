d908dc8e2c17f3886324dcffcfa34a568411bf23
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-05-11 23:01:26 +0900

    210511

--- CLI/x-mas/codeit/크롤링/18_headless_chrome_useragent.py
+++ CLI/x-mas/codeit/크롤링/18_headless_chrome_useragent.py
@@ -3,9 +3,17 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.headless = True
 options.add_argument("window-size=1920x1000")
+options.add_argument("user-agent= Mozilla/5.0 (Window NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84/0.4147.89 Safari/537.36")
 
 browser = webdriver.Chrome(options=options)
 browser.maximize_window()
 
 url = "https://www.whatismybrowser/detect/what-is-my-user-agent"
 browser.get(url)
+
+# Mozilla/5.0 (Window NT 10.0; Win64; x64) 
+# AppleWebKit/537.36 (KHTML, like Gecko) 
+# Chrome/84/0.4147.89 Safari/537.36
+detected_value = browser.find_element_by_id("detected_value")
+print(detected_value.text)
+browser.quit()
