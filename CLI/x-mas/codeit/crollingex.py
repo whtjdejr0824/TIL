0d5427956e18b48d4fa7cab89c1f01f0d9385b39
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-05-14 23:30:49 +0900

    210514

--- CLI/x-mas/codeit/crollingex.py
+++ CLI/x-mas/codeit/crollingex.py
@@ -27,5 +27,17 @@ res = requests.get(url)
 res.raise_for_status()
 soup = BeautifulSoup(res.text, "lxml")
 
-with open("quiz.html", "w", encoding="utf8") as f:
-    f.write(soup.prettify())
+# with open("quiz.html", "w", encoding="utf8") as f:
+    # f.write(soup.prettify())
+
+data_rows = soup.find("table", attrs={"class":"tbl"}).find("tbody").find_all("tr")
+for index, row in enumerate(data_rows):
+    columns = row.find_all("td")
+
+    print("============ 매물 {} ==============".format(index+1))
+    print("거래 :", columns[0].get_text().strip())
+    print("면적 :", columns[1].get_text().strip(), "(공급/전용)")
+    print("가격 :", columns[2].get_text().strip(), "(만원)")
+    print("동 :", columns[3].get_text().strip())
+    print("층 :", columns[4].get_text().strip())
+
