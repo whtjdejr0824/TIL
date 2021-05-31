483388566d0773137f73776e724b718cc626a259
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-05-31 22:16:10 +0900

    210531

--- CLI/x-mas/codeit/크롤링/project.py
+++ CLI/x-mas/codeit/크롤링/project.py
@@ -14,12 +14,21 @@ def scrape_weather():
     res = requests.get(url)
     res.raise_for_status()
     soup = BeautifulSoup(res.text, "lxml")
-    # 흐림, 어제보다 00 높아요.
+    # 흐림, 어제보다 OO 높아요
     cast = soup.find("p", attrs={"class":"cast_txt"}).get_text()
     # 현재 OOC (최저 OO / 최고 OO)
-    curr_temp = soup.find("p", attrs={"class":"info_temperature"}).get_text().replace("도씨", "")
-    min_temp = None
-    max_temp = None
+    curr_temp = soup.find("p", attrs={"class":"info_temperature"}).get_text().replace("도씨", "") #현재 온도
+    min_temp = soup = soup.find("span", attrs={"class":"min"}).get_text # 최저 온도
+    max_temp = soup.find("span", attrs={"class":"max"}).get_text # 최고 온도
+    # 오전 강수확률 OO% / 오후 강수확률 OO%
+    morning_rain_rate = soup.find("span", attrs={"class":"point_time morning"}).get_text().strip() # 오전 강수확률
+    afternoon_rain_rate = soup.find("span", attrs={"class":"point_time afternoon"}).get_text().strip # 오후 강수확률
+
+
+    # 출력
+    print(cast)
+    print("현재 {} (최저 {} / 최고 {})".format(curr_temp, min_temp, max_temp))
+    print("오전 {} / 오후 {}".format(morning_rain_rate,afternoon_rain_rate))
 
 if __name__ == "__main__":
     scrape_weather() # 오늘의 날씨 정보 가져오기
