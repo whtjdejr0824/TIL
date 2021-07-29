277ba80daad9bb57c41c79e23c78b7a847201a96
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-07-29 21:49:03 +0900

    210729

--- CLI/x-mas/codeit/문서자동화/2_mouse_move.py
+++ CLI/x-mas/codeit/문서자동화/2_mouse_move.py
@@ -9,6 +9,13 @@ import pyautogui
 # pyautogui.moveTo(300, 300, duration=0.25)
 
 # 상대 좌표로 마우스 이동 (현재 커서가 있는 위치로부터)
-pyautogui.moveTo(100, 100, duration=0.25)
-pyautogui.move(100, 100, duration=0.25) # 100, 100 기준으로 +100, +100 -> 200, 200
-pyautogui.move(100, 100, duration=0.25) # 200, 200 기준으로 +100, +100 -> 300, 300
+# pyautogui.moveTo(100, 100, duration=0.25)
+# print(pyautogui.position()) # Point(x, y)
+# pyautogui.move(100, 100, duration=0.25) # 100, 100 기준으로 +100, +100 -> 200, 200
+# print(pyautogui.position()) # Point(x, y)
+# pyautogui.move(100, 100, duration=0.25) # 200, 200 기준으로 +100, +100 -> 300, 300
+# print(pyautogui.position()) # Point(x, y)
+
+p = pyautogui.position()
+print(p[0], p[1]) # x, y
+print(p.x, p.y) # x, y
