5552df0caa22478a363395a011b38266a7fbe3a0
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-08-30 22:56:04 +0900

    210830

---
+++ CLI/x-mas/codeit/문서자동화/7_windows.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+fw = pyautogui.getActiveWindow() # 현재 활성화된 창 (VSCode)
+print(fw.title) # 창의 제목 정보
+print(fw.size) # 창의 크기 정보 (width, height)
+print(fw.left, fw.top, fw.right, fw.bottom) # 창의 좌표 정보
+pyautogui.click(fw.left + 25, fw.top + 20)
