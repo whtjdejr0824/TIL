24cf3a5f7a15971773c3ce283ce4fdf3ae10bbef
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-09-06 23:31:45 +0900

    210906

--- CLI/x-mas/codeit/문서자동화/7_windows.py
+++ CLI/x-mas/codeit/문서자동화/7_windows.py
@@ -5,3 +5,6 @@ print(fw.title) # 창의 제목 정보
 print(fw.size) # 창의 크기 정보 (width, height)
 print(fw.left, fw.top, fw.right, fw.bottom) # 창의 좌표 정보
 pyautogui.click(fw.left + 25, fw.top + 20)
+
+for w in pyautogui.getAllWindows():
+  print(w) # 모든 윈도우 가져오기
